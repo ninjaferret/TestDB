19387d33e078fdd2f11db6ab1d440c34642af167
Ian Johnson <ij2030@gmail.com>
2015-09-16 10:17:00 +0100

    Add a second int column

--- Tables/dbo.Test2.sql
+++ Tables/dbo.Test2.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Test2]
 (
 [AnInt] [int] NOT NULL,
-[Text1] [text] COLLATE Latin1_General_CI_AS NULL
+[Text1] [text] COLLATE Latin1_General_CI_AS NULL,
+[SecondInt] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
