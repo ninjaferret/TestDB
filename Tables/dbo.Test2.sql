abdf40e555e42f7d3a768aaab51f390a0b5523e0
Ian Johnson <ij2030@gmail.com>
2015-09-16 10:16:29 +0100

    Fix name of the text1 column

--- Tables/dbo.Test2.sql
+++ Tables/dbo.Test2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Test2]
 (
 [AnInt] [int] NOT NULL,
-[Test] [text] COLLATE Latin1_General_CI_AS NULL
+[Text1] [text] COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
