16445aac3d54bd06b6bc9aed070767a3ba4a449a
Ian Johnson <ian.johnson@red-gate.com>
2015-09-16 09:57:13 +0100

    Adding second int to the table

--- Tables/dbo.Test1.sql
+++ Tables/dbo.Test1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Test1]
 (
 [AnInt] [int] NOT NULL,
-[Test] [text] COLLATE Latin1_General_CI_AS NULL
+[Test] [text] COLLATE Latin1_General_CI_AS NULL,
+[SecondInt] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
