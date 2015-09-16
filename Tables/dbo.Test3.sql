daf5dd799cb3e37634993aaeb8d5d129ef474f60
Ian Johnson <ij2030@gmail.com>
2015-09-16 11:21:31 +0100

    Added a third integer

--- Tables/dbo.Test3.sql
+++ Tables/dbo.Test3.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Test2]
 [AnInt] [int] NOT NULL,
 [Text1] [text] COLLATE Latin1_General_CI_AS NULL,
 [SecondInt] [int] NULL,
+[ThirdInt] [int] NULL,
 [Decimal1] [decimal] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
